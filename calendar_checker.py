f58b4d164c029ec90d2ddf22ce99785011c6685e
WhiskeyWhiskers <stephen.akerson@gmail.com>
2017-03-22 14:16:16 -0700

    Update to use sys.argv

--- calendar_checker.py
+++ calendar_checker.py
@@ -1,4 +1,6 @@
+from logging import info
 import requests
+import sys
 from hamcrest import *
 
 
@@ -6,15 +8,19 @@ CALENDAR_URL = "http://oaklandice.maxgalaxy.net/Schedule.aspx?ID=2"
 
 
 class ScheduleChecker(object):
-    def __init__(self, month, year):
-        self.month = month
-        self.year = year
+    def __init__(self, current_month, next_month):
+        self.current_month = current_month
+        self.next_month = next_month
 
     def check_current_month(self):
         r = requests.get(CALENDAR_URL)
-        assert_that(r.text, contains_string("{month}, {year}".format(month=self.month, year=self.year)))
+        print("{0}: {1}".format(self.current_month, r.text.count(self.current_month)))
+        print("{0}: {1}".format(self.next_month, r.text.count(self.next_month)))
+        assert_that(r.text.count(self.current_month), greater_than(r.text.count(self.next_month)))
+        assert_that(r.text.count(self.next_month), greater_than_or_equal_to(1))
 
 if __name__ == "__main__":
-    s = ScheduleChecker("March", "2017")
+    current_month = sys.argv[1]
+    next_month = sys.argv[2]
+    s = ScheduleChecker(current_month, next_month)
     s.check_current_month()
-
