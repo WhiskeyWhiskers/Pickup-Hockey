87ab8aa1644bb72980826043876bca7b7edd8aff
indigobuffalo <stephen.akerson@gmail.com>
2017-07-01 13:29:20 -0700

    Finish UI

--- calendar_checker.py
+++ calendar_checker.py
@@ -1,8 +1,10 @@
 import requests
 import sys
+import ipdb
 from hamcrest import *
 
 
+LOGIN_URL = "https://oaklandice.maxgalaxy.net/Login.aspx"
 CALENDAR_URL = "http://oaklandice.maxgalaxy.net/Schedule.aspx?ID=2"
 
 
@@ -12,9 +14,37 @@ class ScheduleChecker(object):
         self.next_month = next_month
 
     def check_current_month(self):
-        r = requests.get(CALENDAR_URL)
-        print("{0}: {1}".format(self.current_month, r.text.count(self.current_month)))
-        print("{0}: {1}".format(self.next_month, r.text.count(self.next_month)))
+        payload = {"ctl00$ContentPlaceHolder1$txtUserName": "stephen.akerson@gmail.com",
+                   "ctl00$ContentPlaceHolder1$txtPassword": "ToughCookie11!"}
+        headers = {
+                   "Accept":                      "text/html,application/xhtml+xml,\
+                                                  application/xml;q=0.9,image/webp,*/*;q=0.8",
+                   "Accept-Encoding":             "gzip, deflate, br",
+                   "Accept-Language":             "en-US,en;q=0.8",
+                   "Cache-Control":               "max-age=0",
+                   "Connection":                  "keep - alive",
+                   "Content-Length":              "10576",
+                   "Content-Type":                "application / x - www - form - urlencoded",
+                   "Host":                        "oaklandice.maxgalaxy.net",
+                   "Origin":                      "https: // oaklandice.maxgalaxy.net",
+                   "Referer":                     "https: // oaklandice.maxgalaxy.net / Login.aspx",
+                   "Upgrade-Insecure-Requests":   "1",
+                   "User-Agent":                  "Mozilla / 5.0(Macintosh; Intel Mac OS X 10_12_5)\
+                                                  AppleWebKit / 537.36(KHTML, like Gecko) \
+                                                  Chrome / 58.0.3029.110 Safari / 537.36"
+                   }
+
+        r1 = requests.get(LOGIN_URL)
+        cookie_name = r1.cookies.keys()[0]
+        cookie_val = r1.cookies[cookie_name]
+        payload[cookie_name] = cookie_val
+        ipdb.set_trace()
+        session = requests.Session()
+        resp = session.post(LOGIN_URL, headers=headers, data=payload)
+        print resp.status_code
+        ipdb.set_trace()
+        #print("{0}: {1}".format(self.current_month, r.text.count(self.current_month)))
+        #print("{0}: {1}".format(self.next_month, r.text.count(self.next_month)))
         assert_that(r.text.count(self.current_month), greater_than(r.text.count(self.next_month)))
         assert_that(r.text.count(self.next_month), greater_than_or_equal_to(1))
 
--- calendar_checker_selenium.py
+++ calendar_checker_selenium.py
@@ -1,14 +1,44 @@
 from selenium import webdriver
-import ipdb
+from hamcrest import assert_that, contains_string
+import sys
+import os
 
-URL = 'http://oaklandice.maxgalaxy.net/BrowseDayCamps.aspx'
-driver = webdriver.Chrome()
-driver.get(URL)
 
-drop_in_with_gretzky_caret = driver.find_element_by_css_selector("#ContentPlaceHolder1_sideMenuTreeViewn0 > img")
-drop_in_with_gretzky_caret.click()
+EMAIL = os.getenv('OIC_EMAIL')
+PW = os.getenv('OIC_PW')
+LOGIN_URL = 'http://oaklandice.maxgalaxy.net/BrowseDayCamps.aspx'
+CAL_URL = 'http://oaklandice.maxgalaxy.net/DayCampSelection.aspx?DayCampID={}'
 
-drop_in_link = driver.find_element_by_css_selector("#ContentPlaceHolder1_sideMenuTreeViewt1")
-drop_in_link.click()
 
-ipdb.set_trace()
+class ScheduleChecker(object):
+    def __init__(self, next_month_id):
+        self.next_camp_url = CAL_URL.format(next_month_id)
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(10)
+
+    def check_next_month(self):
+        self.driver.get(LOGIN_URL)
+
+        login_btn = self.driver.find_element_by_id('cmdLogin')
+        login_btn.click()
+
+        email_box = self.driver.find_element_by_id('ContentPlaceHolder1_txtUserName')
+        pw_box = self.driver.find_element_by_id('ContentPlaceHolder1_txtPassword')
+        login_sbt_btn = self.driver.find_element_by_id('ctl00_ContentPlaceHolder1_cmdSubmit_input')
+        email_box.send_keys(EMAIL)
+        pw_box.send_keys(PW)
+        login_sbt_btn.click()
+
+        try:
+            self.driver.get(self.next_camp_url)
+            select_activity_text = self.driver.find_element_by_id('ContentPlaceHolder1_secUniqueNames').text.lower()
+            assert_that(select_activity_text, contains_string('hockey'))
+            raise Exception("New month is up!")
+        except AssertionError:
+            print "Nothing posted yet."
+
+
+if __name__ == "__main__":
+    next_month_id = sys.argv[1]
+    s = ScheduleChecker(next_month_id)
+    s.check_next_month()
