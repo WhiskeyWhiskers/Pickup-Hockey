be1290e1432deb8c1c2f7bd3985984e2b0e1c2d2
indigobuffalo <stephen.akerson@gmail.com>
2018-02-16 23:40:31 -0800

    Updates to support backend check

--- calendar_checker.py
+++ calendar_checker.py
@@ -1,55 +1,51 @@
 import requests
 import sys
 import ipdb
-from hamcrest import *
+from data.form_data import LOGIN_FORM_DATA, LOGOUT_FORM_DATA
 
-
-LOGIN_URL = "https://oaklandice.maxgalaxy.net/Login.aspx"
-CALENDAR_URL = "http://oaklandice.maxgalaxy.net/Schedule.aspx?ID=2"
-
+CAL_URL = 'https://oaklandice.maxgalaxy.net/DayCampSelection.aspx?DayCampID={MONTH_ID}'
+COOKIE = {'ASP.NET_SessionId': 'y2sovzczlybdoc0wajkpfjr0'}
 
 class ScheduleChecker(object):
-    def __init__(self, current_month, next_month):
-        self.current_month = current_month
-        self.next_month = next_month
+    def __init__(self, month_id):
+        self.cal_url = CAL_URL.format(MONTH_ID=month_id)
+        self.login_url = 'https://oaklandice.maxgalaxy.net/Login.aspx'
+        self.session = self.create_session()
+
+    def create_session(self):
+        s = requests.Session()
+        s.headers.update({
+            'Upgrade-Insecure-Requests': '1',
+            'Cache-Control'            : 'max-age=0',
+            'Content-Type'             : 'application/x-www-form-urlencoded',
+            'User-Agent'               : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36',
+            'Referer'                  : 'https://oaklandice.maxgalaxy.net/Login.aspx',
+            'Accept'                   : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'Accept-Encoding'          : 'gzip, deflate, br',
+            'Host'                     : 'oaklandice.maxgalaxy.net',
+            'Accept-Language'          : 'en-US,en;q=0.9',
+            'Cookie'                   : 'ASP.NET_SessionId=y2sovzczlybdoc0wajkpfjr0; BIGipServer~maxgalaxy~maxgalaxy_lvprod_leg_http=3701503754.20480.0000'
+        })
+        cj = requests.utils.cookiejar_from_dict(COOKIE)
+        s.cookies = cj
+        return s
+
+    def login(self):
+        self.session.post(url=self.login_url, data=LOGIN_FORM_DATA)
+
+    def logout(self):
+        self.session.post(url=self.cal_url, data=LOGOUT_FORM_DATA)
 
     def check_current_month(self):
-        payload = {"ctl00$ContentPlaceHolder1$txtUserName": "stephen.akerson@gmail.com",
-                   "ctl00$ContentPlaceHolder1$txtPassword": "ToughCookie11!"}
-        headers = {
-                   "Accept":                      "text/html,application/xhtml+xml,\
-                                                  application/xml;q=0.9,image/webp,*/*;q=0.8",
-                   "Accept-Encoding":             "gzip, deflate, br",
-                   "Accept-Language":             "en-US,en;q=0.8",
-                   "Cache-Control":               "max-age=0",
-                   "Connection":                  "keep - alive",
-                   "Content-Length":              "10576",
-                   "Content-Type":                "application / x - www - form - urlencoded",
-                   "Host":                        "oaklandice.maxgalaxy.net",
-                   "Origin":                      "https: // oaklandice.maxgalaxy.net",
-                   "Referer":                     "https: // oaklandice.maxgalaxy.net / Login.aspx",
-                   "Upgrade-Insecure-Requests":   "1",
-                   "User-Agent":                  "Mozilla / 5.0(Macintosh; Intel Mac OS X 10_12_5)\
-                                                  AppleWebKit / 537.36(KHTML, like Gecko) \
-                                                  Chrome / 58.0.3029.110 Safari / 537.36"
-                   }
-
-        r1 = requests.get(LOGIN_URL)
-        cookie_name = r1.cookies.keys()[0]
-        cookie_val = r1.cookies[cookie_name]
-        payload[cookie_name] = cookie_val
-        ipdb.set_trace()
-        session = requests.Session()
-        resp = session.post(LOGIN_URL, headers=headers, data=payload)
-        print resp.status_code
-        ipdb.set_trace()
-        #print("{0}: {1}".format(self.current_month, r.text.count(self.current_month)))
-        #print("{0}: {1}".format(self.next_month, r.text.count(self.next_month)))
-        assert_that(r.text.count(self.current_month), greater_than(r.text.count(self.next_month)))
-        assert_that(r.text.count(self.next_month), greater_than_or_equal_to(1))
+        r_cal = self.session.get(self.cal_url)
+        if 'does not' in r_cal.text:
+            print("Nothing posted yet")
+        else:
+            print("New spots are up!")
 
 if __name__ == "__main__":
-    current_month = sys.argv[1]
-    next_month = sys.argv[2]
-    s = ScheduleChecker(current_month, next_month)
+    month = sys.argv[1]
+    s = ScheduleChecker(month)
+    s.login()
     s.check_current_month()
+    s.logout()
