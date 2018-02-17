cbb863a045c8bc280bc2402b95b8a38bfca8df31
indigobuffalo <stephen.akerson@gmail.com>
2018-02-17 11:14:10 -0800

    remove unused headers, general cleanup

--- checker.py
+++ checker.py
@@ -1,48 +1,50 @@
-#!/usr/bin/env python
+#!/Users/Akerson/.virtualenvs/pickup-hockey/bin/python
+
+import os
+import sys
+import urllib
 
 import requests
-import sys
-from data.form_data import LOGIN_FORM_DATA, LOGOUT_FORM_DATA
 
-CAL_URL = 'https://oaklandice.maxgalaxy.net/DayCampSelection.aspx?DayCampID={MONTH_ID}'
-COOKIE = {'ASP.NET_SessionId': 'y2sovzczlybdoc0wajkpfjr0'}
+from data.posts import LOGIN_FORM_DATA, LOGOUT_FORM_DATA
+
 
 class ScheduleChecker(object):
     def __init__(self, month_id):
-        self.cal_url = CAL_URL.format(MONTH_ID=month_id)
+        self.cal_url = 'https://oaklandice.maxgalaxy.net/DayCampSelection.aspx?DayCampID=%s' % month_id
         self.login_url = 'https://oaklandice.maxgalaxy.net/Login.aspx'
+
+        self.month_ready_msg = 'Drop-In Hockey'
+        self.month_unready_msg = 'Registration does not fall within'
+        self.logged_out_msg = 'You must be logged in to register'
+
+        self.pw = urllib.quote(os.getenv('OIC_PW'))
+        self.email = urllib.quote(os.getenv('OIC_EMAIL'))
+
         self.session = self.create_session()
+        self.login_form_data = LOGIN_FORM_DATA.format(OIC_PW=self.pw, OIC_EMAIL=self.email)
 
     def create_session(self):
         s = requests.Session()
-        s.headers.update({
-            'Upgrade-Insecure-Requests': '1',
-            'Cache-Control'            : 'max-age=0',
-            'Content-Type'             : 'application/x-www-form-urlencoded',
-            'User-Agent'               : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36',
-            'Referer'                  : 'https://oaklandice.maxgalaxy.net/Login.aspx',
-            'Accept'                   : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-            'Accept-Encoding'          : 'gzip, deflate, br',
-            'Host'                     : 'oaklandice.maxgalaxy.net',
-            'Accept-Language'          : 'en-US,en;q=0.9',
-            'Cookie'                   : 'ASP.NET_SessionId=y2sovzczlybdoc0wajkpfjr0; BIGipServer~maxgalaxy~maxgalaxy_lvprod_leg_http=3701503754.20480.0000'
-        })
-        cj = requests.utils.cookiejar_from_dict(COOKIE)
-        s.cookies = cj
+        s.headers.update({'Content-Type': 'application/x-www-form-urlencoded'})
         return s
 
     def login(self):
-        self.session.post(url=self.login_url, data=LOGIN_FORM_DATA)
+        self.session.post(url=self.login_url, data=self.login_form_data)
 
     def logout(self):
         self.session.post(url=self.cal_url, data=LOGOUT_FORM_DATA)
 
     def check_current_month(self):
         r_cal = self.session.get(self.cal_url)
-        if 'does not' in r_cal.text:
+        if self.logged_out_msg in r_cal.text:
+            print("Logged out of Oakland Ice!")
+        elif self.month_unready_msg in r_cal.text:
             print("Nothing posted yet")
-        else:
+        elif self.month_ready_msg in r_cal.text:
             print("New spots are up!")
+        else:
+            raise RuntimeError('The dom has changed! Time to update this script...')
 
 if __name__ == "__main__":
     month = sys.argv[1]
